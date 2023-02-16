353d0b63a8358cd0625614c824c3cfcd9d432e4d
lesnikate <katerina.lesnikov@gmail.com>
2023-02-16 19:39:29 +0300

    v1602

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class StartSelenium {
+    @BeforeMethod
+    public void preCondition(){
+
+    }
+    @Test
+    public void testName(){
+
+    }
+    @AfterMethod
+    public void postCondition(){
+
+    }
+}
